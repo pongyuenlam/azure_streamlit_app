e374e23ffb6d83ebcf8e589285452d14bc2c9005
pongyuenlam <121112796+pongyuenlam@users.noreply.github.com>
2024-12-25 16:46:38 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
-import os 
-from dotenv import load_dotenv
-load_dotenv()
-#basedir = os.path.abspath(os.path.dirname(__file__))
-#load_dotenv(os.path.join(basedir, '.env'))
-#PASSWORD_APP = os.getenv("PASSWORD_APP")
 import streamlit as st
 st.text_input("Please enter the password to use this App", key="in_password", type="password")
 app_password = st.session_state.in_password
 if app_password:
-    if (app_password != "abc"):
+    if (app_password != PASSWORD_APP):
         st.write("Password incorrect, please try again")
     else:
         st.write("password correct")
